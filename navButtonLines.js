5c2baf8da091925638d30bb7ede0779adcd09a6d
krisjanis-a <krishjanis96@gmail.com>
2021-08-28 16:43:28 +0300

    about section p width fixed

--- navButtonLines.js
+++ navButtonLines.js
@@ -4,13 +4,13 @@ const line3 = document.querySelector("#line-3");
 
 function toggleNavButtonLines() {
   if (menuActive) {
-    console.log("active works");
+    // console.log("active works");
     line1.classList.add("active-line");
     line2.classList.add("active-line");
     line3.classList.add("active-line");
   }
   if (!menuActive) {
-    console.log("inactive works");
+    // console.log("inactive works");
     line1.classList.remove("active-line");
     line2.classList.remove("active-line");
     line3.classList.remove("active-line");
